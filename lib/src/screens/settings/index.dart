5a7c5a5a1ee73639958f89f2f2dd5707c30a1637
Aleksey Ramzaev <aleksei_ramzaev@epamc.om>
2021-05-16 14:44:45 +0400

    screen setting

---
+++ lib/src/screens/settings/index.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Settings'),
+          ],
+        ),
+      ),
+    );
+  }
+}
